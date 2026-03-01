24a34a2b8b3b47ca42de6c493869c1d3cb8df499
gustavo <gustavo24104@gmail.com>
2026-03-01 03:37:51 -0300

    funcao de popular o grafo

--- main.cpp
+++ main.cpp
@@ -9,12 +9,9 @@ using namespace std;
 
 vector<int> CoberturaAproximada(list<int> adj[], int v, bool visitado[]) {
     vector<int> retorno;
-
     for(int i = 0; i < v; ++i) {
-
         if(!visitado[i]) {
             for(int &j : adj[i]) {
-
                 if(!visitado[j]) {
                     visitado[i] = true;
                     visitado[j] = true;
@@ -33,17 +30,24 @@ void colocaAresta(list<int> adj[], int u, int v) {
     adj[v].push_back(u);
 }
 
+// adiciona arestas aleatórias no grafo
+void PopulaGrafo(list<int> adj[], int v) {
+    srand(time(NULL));
+    for(int i = 0; i < v; ++i) {
+        for(int j = i + 1; j < v; ++j) {
+            if(rand() % 100 <= 30) colocaAresta(adj, i, j);
+        }
+    }
+}
+
 
 int main() {
+
+
     bool v[7] = {false};
     list<int> adj[7];
     vector<int> resposta;
-    colocaAresta(adj, 0, 1);
-    colocaAresta(adj, 0, 2);
-    colocaAresta(adj, 1, 3);
-    colocaAresta(adj, 3, 4);
-    colocaAresta(adj, 4, 5);
-    colocaAresta(adj, 5, 6);
+    PopulaGrafo(adj, 7);
 
     resposta = CoberturaAproximada(adj, 7, v);
 
